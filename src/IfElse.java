1d134db201a210af47353759221890f2f3bf6b48
robinsardana55 <robinsardana55@gmail.com>
2023-02-05 15:33:29 -0500

    Worked on two more program and updating them on github.

---
+++ src/IfElse.java
@@ -0,0 +1,7 @@
+public class IfElse {
+    public static void main(String[] args) {
+        System.out.println("The first if else program");
+
+
+    }
+}
---
+++ src/Swapping.java
@@ -0,0 +1,16 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int x, y, z;
+        x = 10;
+        y = 20;
+        System.out.println("The value swapping program");
+        System.out.println("The value of x = " + x);
+        System.out.println("The value of y = " + y);
+
+        z = x;
+        x = y;
+        System.out.println("The value after swapping");
+        System.out.println("The Value of x = " + x);
+        System.out.println("The value of y = " + z);
+    }
+}
