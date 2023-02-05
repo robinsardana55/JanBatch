97aad7fd116ece4370c45761b530517b2bae51d8
robinsardana55 <robinsardana55@gmail.com>
2023-02-05 16:15:38 -0500

    If else programm using a scanner

--- src/JAVA/Scanner.java
+++
@@ -1,4 +0,0 @@
-package JAVA;
-
-public class Scanner {
-}
---
+++ src/JAVA/Sccanner.java
@@ -0,0 +1,17 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class Sccanner {
+    public static void main(String[] args) {
+        System.out.println("Enter the value for x");
+        Scanner value = new Scanner(System.in);
+        int x = value.nextInt();
+
+        if (x < 10) {
+            System.out.println("The value of x is less than 10");
+        } else {
+            System.out.println("The value of x is other than the provided input i.e = " + x);
+        }
+    }
+}
