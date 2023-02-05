839d2e51cdbea51896629527a4845b70ffc8fc1d
robinsardana55 <robinsardana55@gmail.com>
2023-02-05 15:44:15 -0500

    Third program

--- src/IfElse.java
+++ src/IfElse.java
@@ -1,7 +1,13 @@
 public class IfElse {
     public static void main(String[] args) {
-        System.out.println("The first if else program");
+        System.out.println("Enter any number ");
+        int x = 11;
 
+        if (x >= 1 && x <= 10) {
+            System.out.println("The value of x is between 1 and 10 both inclusive");
+        } else {
+            System.out.println("The value of x is not between 1 and 10" + x);
 
+        }
     }
 }
