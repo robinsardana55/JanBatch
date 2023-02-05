64920221844d658b4c7211e10e5fe375aaa7b396
robinsardana55 <robinsardana55@gmail.com>
2023-02-05 16:26:15 -0500

    Program to test while loop

---
+++ src/JAVA/Tableof2.java
@@ -0,0 +1,15 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class Tableof2 {
+    public static void main(String[] args) {
+        System.out.println("Enter your desired number up to 100 ");
+        Scanner table = new Scanner(System.in);
+        int x=table.nextInt();
+        while (x<=100){
+            System.out.println(x + "*" +x+"=" + x*2);
+            x++;
+        }
+    }
+}
