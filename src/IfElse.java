088a010969373ef205d5c8ef1226a89bdf6de724
robinsardana55 <robinsardana55@gmail.com>
2023-02-05 15:48:25 -0500

    Tried to fix the error by changing the path

--- src/IfElse.java
+++ src/IfElse.java
@@ -3,7 +3,7 @@ public class IfElse {
         System.out.println("Enter any number ");
         int x = 11;
 
-        if (x >= 1 && x <= 10) {
+        if ( x >= 1 && x <= 10) {
             System.out.println("The value of x is between 1 and 10 both inclusive");
         } else {
             System.out.println("The value of x is not between 1 and 10" + x);
