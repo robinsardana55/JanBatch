0c2c22a6af89354e9330b33871a7c11c9bccee2a
robinsardana55 <robinsardana55@gmail.com>
2023-02-07 22:47:21 -0500

    forgot to add do while

--- src/JAVA/DoWhile.java
+++ src/JAVA/DoWhile.java
@@ -1,2 +1,15 @@
-package JAVA;public class DoWhile {
+package JAVA;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        System.out.println("Do while loop");
+        int x=10;
+
+        do {
+            x++;
+        }        while (x>5);{
+            System.out.println("x is greater than 5");
+        }
+
+    }
 }
