1e12563baa181c0c14b8de91ba62b2992adaafe6
robinsardana55 <robinsardana55@gmail.com>
2023-02-08 22:11:08 -0500

    Assignment

---
+++ src/assignment/Factorial.java
@@ -0,0 +1,14 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.print("Enter the number ");
+
+        Scanner sc = new Scanner(System.in);
+
+        int fact = 5;
+        int x = 1;
+    }
+}
